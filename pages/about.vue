18f4778c6821ef31fe568bfbfc1f580c7c8fbf4c
Josh Burd <Jburdt@gmail.com>
2023-10-12 12:49:03 -0400

    added NuxtLinks and headers to index and about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <li><NuxtLink to="/">Nuxt Dojo</NuxtLink></li>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
     <h2>About</h2>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
@@ -17,13 +27,13 @@
 </template>
 
 <script setup></script>
- 
+
 <style scoped>
- h2 {
-    margin-bottom: 20px;
-    font-size: 36px;
-  }
-  p {
-    margin: 20px 0;
-  }
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
 </style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <li><NuxtLink to="/">Nuxt Dojo</NuxtLink></li>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
     <p>Products</p>
   </div>
 </template>
