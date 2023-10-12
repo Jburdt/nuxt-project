0a42a16667b686ca0712668b4aeb2d9417ac1cab
Josh Burd <Jburdt@gmail.com>
2023-10-11 23:23:01 -0400

    Added routes to Vue app

---
+++ pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+      reprehenderit quod atque iure commodi? Similique eos dignissimos deleniti
+      sunt. Ratione eligendi incidunt repudiandae exercitationem sit quaerat,
+      soluta molestias eos illo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+      reprehenderit quod atque iure commodi? Similique eos dignissimos deleniti
+      sunt. Ratione eligendi incidunt repudiandae exercitationem sit quaerat,
+      soluta molestias eos illo.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+ 
+<style scoped>
+ h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
