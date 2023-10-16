b7205a308af515a41189b4dfd3b904ff27d3e3e5
Josh Burd <Jburdt@gmail.com>
2023-10-16 14:41:10 -0400

    added error pages

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -5,17 +5,21 @@
 </template>
 
 <script setup>
-import ProductDetails from '../../components/ProductDetails.vue';
+  import ProductDetails from '../../components/ProductDetails.vue';
 
-const { id } = useRoute().params;
-const uri = 'https://fakestoreapi.com/products/' + id;
+  const { id } = useRoute().params;
+  const uri = 'https://fakestoreapi.com/products/' + id;
 
-//fetch the product
-const { data: product } = await useFetch(uri, {key: id});
+  //fetch the product
+  const { data: product } = await useFetch(uri, {key: id});
 
-definePageMeta({
-  layout: "products",
-});
+  if(!product.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true})
+  }
+
+  definePageMeta({
+    layout: "products",
+  });
 </script>
 
 <style scoped></style>
