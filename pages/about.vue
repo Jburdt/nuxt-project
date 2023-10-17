52b4ec88f68ee87bb4d39b7111cfed54b2b50d3c
Josh Burd <Jburdt@gmail.com>
2023-10-17 12:02:59 -0400

    added Dynamic Server Routes

--- pages/about.vue
+++ pages/about.vue
@@ -18,7 +18,7 @@
 </template>
 
 <script setup>
-const { data } = await useFetch("/api/ninja");
+const { data } = await useFetch('/api/currency/GBP');
 </script>
 
 <style scoped>
--- server/api/ninja.js
+++ server/api/ninja.js
@@ -1,9 +1,4 @@
 export default defineEventHandler(async (event) => {
-  // // handle  query params
-  // const { name } = getQuery(event);
-
-  // // handle post data
-  // const { age } = await readBody(event);
 
   // api call with private key
   const { data } = await $fetch(
